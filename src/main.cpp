ff6dfc2b035c3516fc95756b1ea350079664b0c7
BogusKladik <boguskladik@gmail.com>
2021-08-30 01:35:32 +0300

    Added callbacks for change size and exit window

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,54 @@
 
 #include <iostream>
 
+int windowSizeX = 640;
+int windowSizeY = 480;
+
+void glfwWindowSizeCallBack(GLFWwindow* window, int width, int height){
+    windowSizeX = width;
+    windowSizeY = height;
+    glViewport(0, 0, windowSizeX, windowSizeY);
+}
+
+void glfwKeyCallBack(GLFWwindow* window, int key, int scancode, int action, int mode){
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()){
+        std::cout << "GLFW Init failed!" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(windowSizeX, windowSizeY, "2D Game", NULL, NULL);
     if (!window)
     {
+        std::cout << "GLFW Create window: failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallBack);
+    glfwSetKeyCallback(window, glfwKeyCallBack);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 	
 	if (!gladLoadGL()){
 		std::cout << "Cant load GLAD!" << std::endl;
-		return 1;
+		return -1;
 	}
 	
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 	glClearColor(0, 1, 0, 1);
     /* Loop until the user closes the window */
